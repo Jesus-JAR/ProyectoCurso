e9764eb28dfeafd8e92a1725caae85ce5cb4851f
Jesus Arraras <jesusarraras1@gmail.com>
2024-05-08 11:42:55 +0200

    admin añadido

--- ProyectoDjango/AplicationTest/principal/admin.py
+++ ProyectoDjango/AplicationTest/principal/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from models import Tematicas, Autores, Articulos
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nombre','apellido', 'website')
+    list_editable = ('nombre','apellido', 'website')
+    list_filter = ('apellido')
+    search_fields = ['apellido', 'website']
+    readonly_fields = ()
+
+# preguntar
+admin.site.register(Autores,AutorAdmin)
+admin.site.register(Tematicas)
+admin.site.register(Articulos)
