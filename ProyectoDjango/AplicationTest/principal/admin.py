7cee916d8ca48fd566ce850326c6544a79baa47c
Jesus Arraras <jesusarraras1@gmail.com>
2024-05-08 12:40:05 +0200

    principal

--- ProyectoDjango/AplicationTest/principal/admin.py
+++ ProyectoDjango/AplicationTest/principal/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from models import Tematicas, Autores, Articulos
+from principal.models import Tematicas, Autores, Articulos
 
 class AutorAdmin(admin.ModelAdmin):
     list_display = ('nombre','apellido', 'website')
-    list_editable = ('nombre','apellido', 'website')
-    list_filter = ('apellido')
+    list_editable = ('nombre','apellido')
+    list_filter = ['apellido']
     search_fields = ['apellido', 'website']
     readonly_fields = ()
+    list_display_links = ['website']
 
 # preguntar
 admin.site.register(Autores,AutorAdmin)
